4436540c0955dc98d4fce21303a7329b9a4669e9
cuneytercel23 <108323215+cuneytercel23@users.noreply.github.com>
2025-08-08 00:12:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,7 @@ app.get("/", (req, res, next) => {
   res.send(`Hello this is ${serviceName}`);
 });
 
-app.get("/app2", (req, res, next) => {
-  axios
-    .get("http://app2-service:3002")
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((error) => {
-      console.error("Error:", error.message);
-    });
-});
+
 
 app.listen(3001, () => {
   console.log("Service is up on port 3001 ");
